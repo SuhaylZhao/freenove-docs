acb1e7d61a09c8555a99062eb62c4000dc73bcca
Suhayl <895761900@qq.com>
2024-10-10 18:04:12 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,33 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+# -- Options for PDF output -----------------------------------------------
+ 
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title, author, document class [howto, or owner's choice]).
+pdf_documents = [
+    (master_doc, 'myproject.pdf', 'My Project Documentation', author, 'manual'),
+]
+ 
+# Latex elements for use with the LaTeX writer.
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    'papersize': 'a4paper',
+    
+    # The font size ('10pt', '11pt' or '12pt').
+    'pointsize': '10pt',
+    
+    # Additional stuff for the LaTeX preamble.
+    'preamble': r'''
+        \usepackage[utf8]{inputenc}
+        \usepackage[T1]{fontenc}
+        \usepackage{graphicx}
+        \usepackage{hyperref}
+    ''',
+    
+    # Latex figure (float) alignment
+    'figure_align': 'htbp',
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
